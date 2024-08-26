ca9902381b9282c63267f4a5b35f4a8029512064
SoRa <sonamraisx007@gmail.com>
2024-08-26 15:58:21 +0545

    fixing error

--- Node/index.ts
+++ Node/index.ts
@@ -1,12 +1,12 @@
 import express, { Application } from "express";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 const userRoute = require("./routes/userRoute");
 const transactionRoute = require("./routes/transactionRoute");
 
 dotenv.config();
 const app: Application = express();
 
-const PORT = process.env.PORT;
+const PORT = "jeueu";
 
 //redirecting routes
 app.use("/auth", userRoute);
@@ -17,5 +17,5 @@ app
     console.log(`Server running on port ${PORT}`);
   })
   .on("error", (error: Error) => {
-    throw new Error(error.message);
+    console.log(`${error}`);
   });
