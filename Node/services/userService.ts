362d40d7cdf232bc7c8eb730a6a9c4012d162e7b
SoRa <sonamraisx007@gmail.com>
2024-08-27 15:37:30 +0545

    working on authentication

--- Node/services/userService.ts
+++ Node/services/userService.ts
@@ -1,4 +1,17 @@
 import prisma from "../db";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY = process.env.SECRET_KEY || "your-secret-key";
+
+export const createUserService = async (email: string, password: string) => {
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: password,
+    },
+  });
+};
 
 export const getUsersService = async () => {
   try {
