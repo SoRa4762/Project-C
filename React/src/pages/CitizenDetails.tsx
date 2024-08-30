f5520cf48d5d1ff8d523d4a1e2e293513238c61a
SoRa <sonamraisx007@gmail.com>
2024-08-30 09:56:34 +0545

    IT is what it is

--- React/src/pages/CitizenDetails.tsx
+++ React/src/pages/CitizenDetails.tsx
@@ -1,9 +1,103 @@
+import { useEffect, useState } from "react";
+import { deleteOneCitizen, getAllCitizen } from "@/api/citizen";
+import { CitizenshipInterface } from "@/interfaces/citizenInteface";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { useToast } from "@/components/ui/use-toast";
 
+const CitizenDetails: React.FC = () => {
+  const { toast } = useToast();
+  const [citizenshipDetails, setCitizenshipDetails] = useState([]);
+  const handleDeleteCitizen = async (citizenId: string) => {
+    try {
+      const response = await deleteOneCitizen(citizenId);
+      console.log(response);
+      const updatedCitizenshipDetails = citizenshipDetails.filter(
+        (citizen: CitizenshipInterface) => citizen.citizen_id !== citizenId
+      );
+      setCitizenshipDetails(updatedCitizenshipDetails);
 
-const CitizenDetails:React.FC = () => {
+      toast({
+        title: "Success",
+        description: `Citizen deleted successfully`,
+      });
+    } catch (err) {
+      toast({
+        title: "Error",
+        description: `Citizen Id not found: ${err}`,
+        variant: "destructive",
+      });
+    }
+  };
+
+  useEffect(() => {
+    const getCitizenshipDetails = async () => {
+      const response = await getAllCitizen();
+      setCitizenshipDetails(response?.data.getAllFullCitizenshipInfo);
+    };
+
+    getCitizenshipDetails();
+  }, []);
   return (
-    <div>CitizenDetails</div>
-  )
-}
+    <>
+      <div className="flex items-center gap-2">
+        <Link
+          to={"/home"}
+          className="flex justify-center items-center text-xl h-8 w-8 hover:bg-slate-200 rounded-full"
+        >
+          <MdOutlineKeyboardArrowLeft />
+        </Link>
+        <p className="font-bold text-xl">Citizenship Details</p>
+      </div>
+      <Table>
+        <TableCaption>List of all the citienship details.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Citizen ID</TableHead>
+            <TableHead>National ID Number</TableHead>
+            <TableHead>Passport Number</TableHead>
+            <TableHead>Other Identification</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {citizenshipDetails.map((citizen: CitizenshipInterface) => (
+            <TableRow key={citizen.citizen_id}>
+              <TableCell className="font-medium">
+                {citizen.citizen_id}
+              </TableCell>
+              <TableCell>{citizen.national_id_number}</TableCell>
+              <TableCell>{citizen.passport_number}</TableCell>
+              <TableCell>{citizen.other_identification}</TableCell>
+              <TableCell className="flex gap-2">
+                <Button className="bg-emerald-600 hover:bg-emerald-700">
+                  <FaEdit />
+                </Button>
+                <Button
+                  className="bg-red-600 hover:bg-red-700"
+                  onClick={() => handleDeleteCitizen(citizen.citizen_id)}
+                >
+                  <MdDelete />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
 
-export default CitizenDetails
+export default CitizenDetails;
--- React/src/pages/Home.tsx
+++ React/src/pages/Home.tsx
@@ -6,20 +6,20 @@ const Home = () => {
   return (
     <>
       <div className="h-screen w-full mt-4 sm:px-12">
-        <div className="h-full w-full grid grid-rows-8 sm:grid-cols-2 gap-4 sm:gap-8">
+        <div className="h-full w-full grid grid-rows-3 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-8">
           <Link
             to={"/addcitizen"}
-            className="h-full w-full flex items-center gap-2 border-2 pl-4 hover:bg-emerald-600 hover:text-white border-emerald-600 rounded-md cursor-pointer duration-300"
+            className="h-full w-full flex items-end pb-4 gap-2 border-2 pl-4 hover:bg-emerald-600 hover:text-white border-emerald-600 rounded-3xl cursor-pointer duration-300"
           >
-            <IoPersonAdd fontSize="1.6rem" />{" "}
-            <p className="font-bold">Add Citizen</p>
+            <IoPersonAdd fontSize="1.8rem" />{" "}
+            <p className="font-bold text-xl">Add Citizen</p>
           </Link>
           <Link
             to={"/citizens"}
-            className="h-full w-full flex items-center gap-2 border-2 pl-4 hover:bg-emerald-600 hover:text-white border-emerald-600 rounded-md cursor-pointer duration-300"
+            className="h-full w-full flex items-end pb-4 gap-2 border-2 pl-4 hover:bg-emerald-600 hover:text-white border-emerald-600 rounded-3xl cursor-pointer duration-300"
           >
-            <HiMiniUserGroup fontSize="2rem" />{" "}
-            <p className="font-bold">List Citizens</p>
+            <HiMiniUserGroup fontSize="2.2rem" />{" "}
+            <p className="font-bold text-xl">List Citizens</p>
           </Link>
         </div>
       </div>
